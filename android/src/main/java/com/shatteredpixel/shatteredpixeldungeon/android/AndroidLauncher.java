9cf29b5b2431dda8fd7b88b66bd0706868e34db9
zay448345045 <raknet@163.com>
2025-06-04 19:51:12 +0800

    Update AndroidLauncher.java

--- android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
+++ android/src/main/java/com/shatteredpixel/shatteredpixeldungeon/android/AndroidLauncher.java
@@ -37,7 +37,7 @@ import com.badlogic.gdx.backends.android.AndroidAudio;
 import com.badlogic.gdx.backends.android.AsynchronousAndroidAudio;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeType;
 import com.badlogic.gdx.utils.GdxNativesLoader;
-import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseApp;
 import com.shatteredpixel.shatteredpixeldungeon.SPDSettings;
 import com.shatteredpixel.shatteredpixeldungeon.ShatteredPixelDungeon;
 import com.shatteredpixel.shatteredpixeldungeon.services.news.News;
@@ -65,7 +65,7 @@ public class AndroidLauncher extends AndroidApplication {
 				.minTimeBetweenCrashesMs(2000) //default: 3000
 				.errorActivity(ErrorActivity.class) //default: null (default error activity)
 				.apply();
-		FirebaseApp.initializeApp(this);
+//		FirebaseApp.initializeApp(this);
 		try {
 			GdxNativesLoader.load();
 			FreeType.initFreeType();
